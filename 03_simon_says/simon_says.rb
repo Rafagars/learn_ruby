da8c8eb9f5178ccc8310271b2a415ed0b9577ea8
rafagars <rafagar96@gmail.com>
2019-08-21 14:27:43 -0400

    Exercises 03 and 05 completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -29,5 +29,14 @@ def first_word(string)
 end
 
 def titleize(string)
-	return string.split(/ |\_/).map(&:capitalize).join(" ")
+    string.capitalize!  # capitalize the first word in case it is part of the no words array
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+    phrase = string.split(" ").map {|word| 
+        if words_no_cap.include?(word) 
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ") # I replaced the "end" in "end.join(" ") with "}" because it wasn't working in Ruby 2.1.1
+  phrase  # returns the phrase with all the excluded words
 end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+
+attr_reader :title
+
+def title=(title)
+	title.capitalize!  # capitalize the first word in case it is part of the no words array
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but", "in", "on", "of", "an", "any"]
+    phrase = title.split(" ").map {|word| 
+        if words_no_cap.include?(word) 
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ") # I replaced the "end" in "end.join(" ") with "}" because it wasn't working in Ruby 2.1.1
+	phrase  # returns the phrase with all the excluded words
+	@title = phrase
+end
+
 end
