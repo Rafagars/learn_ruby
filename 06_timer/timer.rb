3d8ca2fed170ef351744d66a47b5ab50530597df
rafagars <rafagar96@gmail.com>
2019-08-21 19:18:14 -0400

    Exercise 06 completed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+  attr_reader :seconds
+
+  def initialize
+  	@timer = Time.mktime(2019,8,21)
+  	@seconds = 0
+  end
+
+  def seconds=(seconds)
+  	@seconds = @timer + seconds
+  end
+
+  def time_string()
+  	@time_string = @seconds.strftime("%H:%M:%S")
+  end
+
 end
